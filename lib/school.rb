bf36157df60c537f4af93667bef01583b9b2bff6
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-07 02:43:40 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,15 +1,16 @@
 # code here!
 class School
   def initialize(name_of_school)
+    @name = name
+    @roster = {}
+  end
 
+  def roster
+    @roster
   end
 
   def add_student(name, grade)
     roster[grade] = [name]
-  end
-
-  def roster
-    @roster = {}
   end
 
   def grade(grade)
