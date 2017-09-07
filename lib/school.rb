2317e05e1c29378b987eedd6a10fd1f3f4cd69ac
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-07 02:35:55 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
 # code here!
+class School
+  def initialize(name_of_school)
+
+  end
+
+  def add_student(name, grade)
+    
+  end
+
+  def roster
+    roster = {}
+  end
+
+  def grade(grade)
+    
+  end
+
+end
