ba89d81a317ae3e5b8f8be9cb5e854e8f0c41eb8
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-07 02:47:01 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,11 @@ class School
   end
 
   def add_student(name, grade)
-    @roster[grade] = [name]
+    if @roster[grade] != nil
+      @roster[grade] << name
+    else
+      @roster[grade] = name
+      
   end
 
   def grade(grade)
